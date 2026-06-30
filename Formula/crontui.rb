0af8c8ed431b9357c6e9cc6b719e6b4862412b21
root <root@Hobbithole-Lab.local>
2026-06-30 20:48:33 +0200

    chore(release): update Homebrew formula for v1.2.0

--- Formula/crontui.rb
+++ Formula/crontui.rb
@@ -1,8 +1,8 @@
 class Crontui < Formula
   desc "Terminal-based visualizer and manager for cronjobs"
   homepage "https://github.com/Piggy90/crontui"
-  url "https://github.com/Piggy90/crontui/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "f6e276640cf4d34e167baac4a367b982f142f1f0b0c4ba8b6937ad5c08ce0b23"
+  url "https://github.com/Piggy90/crontui/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "a0e1d58ada6042a61be4a03d39674a868a49a381d1fab6fa80213b63770dd017"
   license "MIT"
 
   depends_on "python"
